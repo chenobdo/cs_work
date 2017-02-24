e7e41463183a1873a5d6bd2509a187edf7d8c56c
Gabriel <obdo.briel@gmail.com>
2017-02-24 19:00:00 +0800

    [ADD]字节于强制转换

---
+++ 0224/show_bytes.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        printf(" %.2x", start[i]);
+        printf("\n");
+    }
+}
+
+void show_int(int x)
+{
+    show_bytes((byte_pointer) &x, sizeof(int)); // (byte_pointer) &x 这是强制变量类型转换的写法
+}
+
+void show_float(float x)
+{
+    show_bytes((byte_pointer) &x, sizeof(int)); //表达式sizeof(T)返回存储一个类型为T的对象所需要得字节数
+}
+
+void show_pointer(void *x)
+{
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
---
+++ 0224/test.c
@@ -0,0 +1,7 @@
+#include <studio.h>
+
+int x = -1;
+unsigned u = 2147483648;
+
+printf("x = %u = %d\n", x, x);
+printf("u = %u = %d\n", u, u);
