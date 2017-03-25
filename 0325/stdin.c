83ea8dbdf594b7126858457b008e4785a7692ec4
Gabriel <obdo.briel@gmail.com>
2017-03-25 16:33:11 +0800

    stdin

---
+++ 0325/stdin.c
@@ -0,0 +1,16 @@
+#include "csapp.h"
+#define MAXN 15213
+
+in array[MAXN];
+
+int main()
+{
+	int i,n ;
+
+	scanf("%d", &n);
+	if (n > MAXN)
+		app_error("Input file too big");
+	for (i = 0; i < n; i++)
+		scanf("%d", &array[i]);
+	exit(0);
+}
